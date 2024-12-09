99ee1674920c300c422a99060b2e62c2b5610b1b
n-j-p <10066183+n-j-p@users.noreply.github.com>
2024-12-09 12:13:29 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
    version='0.1',
    description='Demonstration of how to create a importable gihub repo',
    author='n-j-p',
-   packages=['pippable_pacakge'],
+   packages=['pippable_package'],
 )
