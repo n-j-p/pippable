e0294c1269ce06beaaa48c92b819c6aaad55294d
n-j-p <10066183+n-j-p@users.noreply.github.com>
2024-12-09 12:03:24 +1100

    Create pippable_package.py

---
+++ pippable_package.py
@@ -0,0 +1,8 @@
+def square(x):
+    return x**2
+  
+class PowerFunc():
+    def __init__(self, exponent):
+        self.exponent = exponent
+    def ans(self, a):
+        return pow(a, self.exponent)
