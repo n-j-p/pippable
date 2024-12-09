46d581d190dfbf268a5ade0acb53594b8475b7c2
n-j-p <10066183+n-j-p@users.noreply.github.com>
2024-12-09 12:11:55 +1100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='pippable_package',
+   version='n-j-p',
+   description='Demonstration of how to create a importable gihub repo',
+   author='n-j-p',
+   packages=['pipable_pacakge'],
+)
